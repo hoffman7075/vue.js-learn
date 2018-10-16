ddf836606a9349025bd95e4d293338f409f03898
hoffman7075 <hoffman.ozr@gmail.com>
2018-10-16 18:20:50 +0500

    Добавил шаблон bootstrap

--- vue-cli-app/src/App.vue
+++ vue-cli-app/src/App.vue
@@ -1,31 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav mr-auto">
+            <router-link tag="li" to="/" class="nav-item">
+              <a class="nav-link">Главная</a>
+            </router-link>
+            <router-link tag="li" to="/users" class="nav-item">
+              <a class="nav-link">Список пользователей</a>
+            </router-link>
+            <router-link tag="li" to="/add" class="nav-item">
+              <a class="nav-link">Добавить пользователя</a>
+            </router-link>
+          </ul>
+        </div>
+      </nav>
+
+      <main role="main" class="container">
+
+        <router-view/>
+
+      </main><!-- /.container -->
   </div>
 </template>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 80px;
 }
 </style>
